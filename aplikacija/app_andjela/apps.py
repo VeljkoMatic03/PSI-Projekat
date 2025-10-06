e951e794e23fd6a91292a6ce1ec4161e0bc25f70
Вељко <mv220283d@student.etf.bg.ac.rs>
2025-10-06 16:42:16 +0200

    Inicijalna verzija projekta - setup
    Change-Id: Ie9037e72933103291ac909196876a000e19ab57c

---
+++ aplikacija/app_andjela/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppAndjelaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_andjela'
---
+++ aplikacija/app_veljko/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppVeljkoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_veljko'
