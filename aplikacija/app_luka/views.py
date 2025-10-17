7765944b551139ab8e40f52d3d45a6206a7c0404
Luka Zdravic <zl220325d@student.etf.bg.ac.rs>
2025-10-17 17:26:48 +0200

    dashboard-student i create-ad.html
    Change-Id: I293333456e84f02e36b625bdad615015d7627946

--- aplikacija/app_luka/views.py
+++ aplikacija/app_luka/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from shared_app.models import Student
+
+
 # Create your views here.
+def create_ad(request):
+    if request.method=="POST":
+        naziv_oglasa=request.POST.get('naziv_oglasa')
+        predmet=request.POST.get('predmet')
+        tip_pomoci=request.POST.get('tip_pomoci')
+        opis=request.POST.get('opis')
+        print(naziv_oglasa)
+        print(predmet)
+        print(tip_pomoci)
+        print(opis)
+    return render(request,'create-ad.html')
+def dashboard_student(request):
+    return render(request,'dashboard-student.html')
