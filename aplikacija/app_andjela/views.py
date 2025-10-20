82f592b3f12a981589415591319074e750948c2b
Andjela <da220132d@student.etf.bg.ac.rs>
2025-10-20 19:20:45 +0200

    Dodato kreiranje cv-a
    Change-Id: I10850766c19c626e4e76e0be34b9a4a7bcefa926

--- aplikacija/app_andjela/views.py
+++ aplikacija/app_andjela/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from shared_app.models import Cv, Tutor, MyUser
+
 
 # Create your views here.
 
@@ -6,6 +9,37 @@ def dashboard_tutor(request):
     return render(request, 'dashboard-tutor.html')
 
 def create_cv(request):
+    if request.method == 'POST':
+        user =  MyUser.objects.filter(username=request.user)
+        print("                     ", user.first().iduser)
+        tutor = Tutor.objects.filter(iduser=user.first().iduser)
+        cv = Cv.objects.filter(idtutor=user.first().iduser)
+
+        if cv:
+            msg = "Vec ste kreirali CV"
+            return render(request, 'create-cv.html', {'msg': msg})
+
+        ime = request.POST['ime']
+        prezime = request.POST['prezime']
+        slika = request.POST['slika']
+        oMeni = request.POST['oMeni']
+        edukacija = request.POST['edukacija']
+        projekti = request.POST['projekti']
+        iskustvo = request.POST['iskustvo']
+
+        Cv.objects.create(
+            name=ime,
+            surname=prezime,
+            picture=slika,
+            aboutme=oMeni,
+            education=edukacija,
+            projects=projekti,
+            experience=iskustvo,
+            idtutor=tutor.first()
+        )
+
+        return redirect('dashboard-tutor')
+
     return render(request, 'create-cv.html')
 
 def edit_cv(request):
