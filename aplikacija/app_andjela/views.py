68473eb03ed1f4b7eef3c2e53ae0b21146826fd5
Andjela <da220132d@student.etf.bg.ac.rs>
2025-10-21 01:40:17 +0200

    Dodato kreiranje i preuzimanje pdf-a
    Change-Id: I49b0efc432a095503ff28cf3bd2c58c13006fc9d

--- aplikacija/app_andjela/views.py
+++ aplikacija/app_andjela/views.py
@@ -1,6 +1,12 @@
 import base64
+from io import BytesIO
 
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.utils import ImageReader
+from reportlab.pdfgen import canvas
 
 from shared_app.models import Cv, Tutor, MyUser
 
@@ -112,4 +118,120 @@ def edit_cv(request):
     return render(request, 'edit-cv.html', context)
 
 def download_cv(request):
-    return render(request, 'dashboard-tutor.html', {'msg': "nemate cv"})
+    user = MyUser.objects.filter(username=request.user)
+    tutor = Tutor.objects.filter(iduser=user.first().iduser)
+    cvs = Cv.objects.filter(idtutor=user.first().iduser)
+    cv = cvs.first()
+
+    if cvs is None or cv is None:
+        return render(request, 'dashboard-tutor.html', {'msg': "nemate cv"})
+
+    name = cv.name
+    surname = cv.surname
+    aboutme = cv.aboutme
+    education = cv.education
+    projects = cv.projects
+    experience = cv.experience
+    picture = cv.picture
+
+    img_reader = None
+    if picture:
+        if isinstance(picture, bytes):
+            picture = picture.decode('utf-8')
+        img_data = base64.b64decode(picture)
+        img_reader = ImageReader(BytesIO(img_data))
+
+    # Priprema PDF fajla
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=\"cv.pdf\"'
+
+    buffer = BytesIO()
+    pdf = canvas.Canvas(buffer, pagesize=A4)
+    width, height = A4
+
+    # --- Dizajn ---
+    margin_left = 60
+    start_y = height - 80
+
+    # Boje
+    title_color = colors.HexColor("#003366")
+    label_color = colors.HexColor("#005599")
+    text_color = colors.black
+
+    # Naslov
+    pdf.setFont("Helvetica-Bold", 24)
+    pdf.setFillColor(title_color)
+    pdf.drawString(margin_left, start_y, "Curriculum Vitae")
+
+    # Linija ispod naslova
+    pdf.setStrokeColor(title_color)
+    pdf.setLineWidth(2)
+    pdf.line(margin_left, start_y - 10, width - margin_left, start_y - 10)
+
+    y = start_y - 70
+
+    # Sekcije
+    sections = [
+        ("Ime:", name),
+        ("Prezime:", surname),
+        ("O meni:", aboutme),
+        ("Edukacija:", education),
+        ("Projekti:", projects),
+        ("Iskustvo:", experience),
+    ]
+
+    pdf.setFont("Helvetica", 12)
+    for label, value in sections:
+        # Labela
+        pdf.setFont("Helvetica-Bold", 13)
+        pdf.setFillColor(label_color)
+        pdf.drawString(margin_left, y, label)
+
+        # Sadržaj (uvođenje)
+        pdf.setFont("Helvetica", 12)
+        pdf.setFillColor(text_color)
+
+        # Prelom teksta ako je duži
+        text_obj = pdf.beginText(margin_left + 100, y)
+        text_obj.setFont("Helvetica", 12)
+        text_obj.setFillColor(text_color)
+        max_width = width - margin_left - 120
+        words = value.split(' ')
+        line = ''
+        count = 0
+        for word in words:
+            test_line = line + word + ' '
+            if pdf.stringWidth(test_line, "Helvetica", 12) < max_width:
+                line = test_line
+            else:
+                text_obj.textLine(line.strip())
+                line = word + ' '
+                count = count + 1
+        text_obj.textLine(line.strip())
+        pdf.drawText(text_obj)
+
+        # Smanji Y za sledeću sekciju
+        y -= 60
+        if count > 1:
+            y -= (count - 1) * 15
+        if y < 100:
+            pdf.showPage()
+            y = height - 100
+
+    # Slika ako postoji
+    if img_reader:
+        pdf.drawImage(img_reader, width - 180, height - 220, width=100, height=100, mask='auto')
+
+    # Footer
+    pdf.setFont("Helvetica-Oblique", 10)
+    pdf.setFillColor(colors.grey)
+    pdf.drawRightString(width - margin_left, 40, "StudyBuddy")
+
+    # Završetak
+    pdf.showPage()
+    pdf.save()
+
+    pdf_data = buffer.getvalue()
+    buffer.close()
+    response.write(pdf_data)
+    return response
